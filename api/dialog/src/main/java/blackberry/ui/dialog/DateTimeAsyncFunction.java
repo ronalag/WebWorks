8415886915efc60c2ff26118e60734b04e739357
ababut <ababut@rim.com>
2011-11-25 10:59:02 -0500

    Moved all navmode related classes to nav subpackage. Refactored dialog creation to new factory class to reduce code duplication.

--- api/dialog/src/main/java/blackberry/ui/dialog/DateTimeAsyncFunction.java
+++ api/dialog/src/main/java/blackberry/ui/dialog/DateTimeAsyncFunction.java
@@ -10,15 +10,11 @@ package blackberry.ui.dialog;
 import blackberry.core.FunctionSignature;
 import blackberry.core.ScriptableFunctionBase;
 
-import net.rim.device.api.script.ScriptEngine;
 import net.rim.device.api.script.Scriptable;
 import net.rim.device.api.script.ScriptableFunction;
 import net.rim.device.api.ui.UiApplication;
 
-import blackberry.common.util.json4j.JSONObject;
-
-import blackberry.ui.dialog.DateTimeDialog;
-import blackberry.ui.dialog.IWebWorksDialog;
+import blackberry.ui.dialog.nav.DialogRunnableFactory;
 
 public class DateTimeAsyncFunction extends ScriptableFunctionBase {
     
@@ -29,53 +25,26 @@ public class DateTimeAsyncFunction extends ScriptableFunctionBase {
     public static final int TYPE_TIME = 2;
     public static final int TYPE_DATETIME = 3;
     public static final int TYPE_DATETIMELOCAL = 4;
-    
-    private ScriptEngine _scriptEngine;
-    
-    public DateTimeAsyncFunction(ScriptEngine se) {
-        _scriptEngine = se;
-    }
 
     /**
      * @see blackberry.core.ScriptableFunctionBase#execute(Object, Object[])
      */
     public Object execute(Object thiz, Object[] args) throws Exception {
-        String typeStr = (String) args[0];
+        String type = (String) args[0];
         Scriptable options = (Scriptable) args[1];
-        String callback = (String) args[2] ;
+        ScriptableFunction callback = (ScriptableFunction) args[2] ;
         
-        int type = getTypeForStringArg(typeStr);
         String value = (String)options.getField("value");
         String min = (String)options.getField("min");
         String max = (String)options.getField("max");
-        String stepStr = (String)options.getField("step");
-        double step = "".equals(stepStr) ? 0 : Double.parseDouble(stepStr);
         
-        // create dialog
-        DateTimeDialog d = new DateTimeDialog(type, value, min, max, step);
-        DialogRunner currentDialog = new DialogRunner(d, _scriptEngine, callback);
-
+        Runnable dr = DialogRunnableFactory.getDateTimeRunnable(type, value, min, max, callback, thiz);
+        
         // queue
-        UiApplication.getUiApplication().invokeLater(currentDialog);
+        UiApplication.getUiApplication().invokeLater(dr);
 
         // return value
         return Scriptable.UNDEFINED;
-    }
-    
-    private int getTypeForStringArg(String typeStr) {
-         if("date".equals(typeStr)) {
-            return TYPE_DATE;
-         } else if("month".equals(typeStr)) {
-            return TYPE_MONTH;
-         } else if("time".equals(typeStr)) {
-            return TYPE_TIME; 
-         } else if("datetime".equals(typeStr)) {
-            return TYPE_DATETIME; 
-         } else if("local".equals(typeStr)) {
-            return TYPE_DATETIMELOCAL; 
-         } else {
-            throw new IllegalArgumentException("Invalid date type.");
-         }
     }
     
     /**
@@ -88,46 +57,8 @@ public class DateTimeAsyncFunction extends ScriptableFunctionBase {
         // options
         fs.addParam(Scriptable.class, true);
         //callback
-        fs.addParam(String.class, true);
+        fs.addParam(ScriptableFunction.class, true);
         
         return new FunctionSignature[] { fs };
-    }
-
-    private static class DialogRunner implements Runnable {
-        private IWebWorksDialog _dialog;
-        private ScriptEngine _se;
-        private String _callback;
-            
-        /**
-         * Constructs a <code>DialogRunner</code> object.
-         * 
-         * @param dialog
-         *            The dialog
-         * @param callback
-         *            The onSelect callback
-         */
-        DialogRunner(IWebWorksDialog dialog, ScriptEngine se, String callback) {
-            _dialog = dialog;
-            _se = se;
-            _callback = callback;
-        }
-        
-
-        /**
-         * Run the dialog.
-         * 
-         * @see java.lang.Runnable#run()
-         */
-        public void run() { 
-            if(_dialog.show()) {
-                final String newDate = _dialog.getSelectedValue();
-                
-                try {
-                    _se.executeScript(_callback + "(\"" + newDate + "\");", null);
-                } catch (Exception e) {
-                    throw new RuntimeException("Invoke callback failed: " + e.getMessage());
-                }
-            }
-        }
     }
 }
--- api/dialog/src/main/java/blackberry/ui/dialog/SelectAsyncFunction.java
+++ api/dialog/src/main/java/blackberry/ui/dialog/SelectAsyncFunction.java
@@ -15,17 +15,14 @@
  */
 package blackberry.ui.dialog;
 
-import java.util.Vector;
-
 import net.rim.device.api.script.ScriptEngine;
 import net.rim.device.api.script.Scriptable;
-import net.rim.device.api.script.ScriptableFunction;
 import net.rim.device.api.ui.UiApplication;
-
+import blackberry.common.util.json4j.JSONArray;
 import blackberry.core.FunctionSignature;
 import blackberry.core.ScriptableFunctionBase;
 
-import blackberry.common.util.json4j.JSONArray;
+import blackberry.ui.dialog.nav.SelectDialog;
 
 /**
  * Implementation of asynchronous selection dialog
@@ -36,56 +33,58 @@ import blackberry.common.util.json4j.JSONArray;
 public class SelectAsyncFunction extends ScriptableFunctionBase {
 
     public static final String NAME = "selectAsync";
-    
+
     public static final int POPUP_ITEM_TYPE_OPTION = 0;
     public static final int POPUP_ITEM_TYPE_GROUP = 1;
     public static final int POPUP_ITEM_TYPE_SEPARATOR = 2;
-    
+
     private ScriptEngine _scriptEngine;
-    
-    public SelectAsyncFunction(ScriptEngine se) {
+
+    public SelectAsyncFunction( ScriptEngine se ) {
         _scriptEngine = se;
     }
 
     /**
      * @see blackberry.core.ScriptableFunctionBase#execute(Object, Object[])
      */
-    public Object execute(Object thiz, Object[] args) throws Exception {
-        boolean allowMultiple = ((Boolean) args[0]).booleanValue();
-        Scriptable choices = (Scriptable) args[1];
-        String callback = (String) args[2] ;
-        
+    public Object execute( Object thiz, Object[] args ) throws Exception {
+        boolean allowMultiple = ( (Boolean) args[ 0 ] ).booleanValue();
+        Scriptable choices = (Scriptable) args[ 1 ];
+        String callback = (String) args[ 2 ];
+
         int numChoices = choices.getElementCount();
-        String[] labels = new String[numChoices];
-        boolean[] enabled = new boolean[numChoices];
-        boolean[] selected = new boolean[numChoices]; 
-        int[] types = new int[numChoices];
-        
-        populateChoiceStateArrays(choices, labels, enabled, selected, types);
+        String[] labels = new String[ numChoices ];
+        boolean[] enabled = new boolean[ numChoices ];
+        boolean[] selected = new boolean[ numChoices ];
+        int[] types = new int[ numChoices ];
+
+        populateChoiceStateArrays( choices, labels, enabled, selected, types );
 
         // create dialog
-        SelectDialog d = new SelectDialog(allowMultiple, labels, enabled, selected, types);
-        SelectDialogRunner currentDialog = new SelectDialogRunner(d, _scriptEngine, callback);
+        SelectDialog d = new SelectDialog( allowMultiple, labels, enabled, selected, types );
+        SelectDialogRunner currentDialog = new SelectDialogRunner( d, _scriptEngine, callback );
 
         // queue
-        UiApplication.getUiApplication().invokeLater(currentDialog);
+        UiApplication.getUiApplication().invokeLater( currentDialog );
 
         // return value
         return Scriptable.UNDEFINED;
     }
-    
-    private void populateChoiceStateArrays(Scriptable fromScriptableChoices, String[] labels, boolean[] enabled, boolean[] selected, int[] type) {
+
+    private void populateChoiceStateArrays( Scriptable fromScriptableChoices, String[] labels, boolean[] enabled,
+            boolean[] selected, int[] type ) {
         try {
-            
-            for(int i = 0; i < fromScriptableChoices.getElementCount(); i++) {
-                Scriptable choice = (Scriptable)fromScriptableChoices.getElement(i);
-                labels[i] = (String)choice.getField("label");
-                enabled[i] = ((Boolean)choice.getField("enabled")).booleanValue();
-                selected[i] = ((Boolean)choice.getField("selected")).booleanValue();
-                type[i] = ((String)choice.getField("type")).equals("group") ? POPUP_ITEM_TYPE_GROUP : POPUP_ITEM_TYPE_OPTION;
+
+            for( int i = 0; i < fromScriptableChoices.getElementCount(); i++ ) {
+                Scriptable choice = (Scriptable) fromScriptableChoices.getElement( i );
+                labels[ i ] = (String) choice.getField( "label" );
+                enabled[ i ] = ( (Boolean) choice.getField( "enabled" ) ).booleanValue();
+                selected[ i ] = ( (Boolean) choice.getField( "selected" ) ).booleanValue();
+                type[ i ] = ( (String) choice.getField( "type" ) ).equals( "group" ) ? POPUP_ITEM_TYPE_GROUP
+                        : POPUP_ITEM_TYPE_OPTION;
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
+        } catch( Exception e ) {
+            throw new RuntimeException( e.getMessage() );
         }
     }
 
@@ -93,14 +92,14 @@ public class SelectAsyncFunction extends ScriptableFunctionBase {
      * @see blackberry.core.ScriptableFunctionBase#getFunctionSignatures()
      */
     protected FunctionSignature[] getFunctionSignatures() {
-        FunctionSignature fs = new FunctionSignature(3);
+        FunctionSignature fs = new FunctionSignature( 3 );
         // allowMultiple
-        fs.addParam(Boolean.class, true);
+        fs.addParam( Boolean.class, true );
         // choices
-        fs.addParam(Scriptable.class, true);
-        //callback
-        fs.addParam(String.class, true);
-        
+        fs.addParam( Scriptable.class, true );
+        // callback
+        fs.addParam( String.class, true );
+
         return new FunctionSignature[] { fs };
     }
 
@@ -108,7 +107,7 @@ public class SelectAsyncFunction extends ScriptableFunctionBase {
         private SelectDialog _dialog;
         private ScriptEngine _se;
         private String _callback;
-            
+
         /**
          * Constructs a <code>DialogRunner</code> object.
          * 
@@ -117,40 +116,39 @@ public class SelectAsyncFunction extends ScriptableFunctionBase {
          * @param callback
          *            The onSelect callback
          */
-        SelectDialogRunner(SelectDialog dialog, ScriptEngine se, String callback) {
-                _dialog = dialog;
-                _se = se;
-                _callback = callback;
+        SelectDialogRunner( SelectDialog dialog, ScriptEngine se, String callback ) {
+            _dialog = dialog;
+            _se = se;
+            _callback = callback;
         }
-        
 
         /**
          * Run the dialog.
          * 
          * @see java.lang.Runnable#run()
          */
-        public void run() { 
-            UiApplication.getUiApplication().pushModalScreen(_dialog);
-            
+        public void run() {
+            UiApplication.getUiApplication().pushModalScreen( _dialog );
+
             int[] newSelectedItems = _dialog.getResponse();
-            
-            if(newSelectedItems != null) {
+
+            if( newSelectedItems != null ) {
                 try {
-                    final String jsCallbackArgs = (new JSONArray(boxIntArray(newSelectedItems))).toString();
-                    _se.executeScript(_callback + "(" + jsCallbackArgs + ");", null);
-                } catch (Exception e) {
-                    throw new RuntimeException("Invoke callback failed: " + e.getMessage());
+                    final String jsCallbackArgs = ( new JSONArray( boxIntArray( newSelectedItems ) ) ).toString();
+                    _se.executeScript( _callback + "(" + jsCallbackArgs + ");", null );
+                } catch( Exception e ) {
+                    throw new RuntimeException( "Invoke callback failed: " + e.getMessage() );
                 }
             }
         }
-        
-        private Integer[] boxIntArray(int[] ints) {
-            Integer[] boxed = new Integer[ints.length];
-            
-            for(int i = 0; i < ints.length; i++) {
-                boxed[i] = new Integer(ints[i]);
+
+        private Integer[] boxIntArray( int[] ints ) {
+            Integer[] boxed = new Integer[ ints.length ];
+
+            for( int i = 0; i < ints.length; i++ ) {
+                boxed[ i ] = new Integer( ints[ i ] );
             }
-            
+
             return boxed;
         }
     }
